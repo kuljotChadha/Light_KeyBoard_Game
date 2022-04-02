88ef761eecdd2bac0041c2aed4fb5dd42f16c2fb
kuljot Singh <kuljotsingh30@gmail.com>
2022-04-01 23:25:42 -0400

    BASIC

---
+++ README.md
@@ -0,0 +1,79 @@
+# Pre-work - *Its Game Time*
+
+**Its Game Time** is a Light & Sound Memory game to apply for CodePath's SITE Program. 
+
+Submitted by: **Kuljot Singh**
+
+Time spent: **10** hours spent in total
+
+Link to project: https://glitch.com/edit/#!/its-game-time-
+
+## Required Functionality
+
+The following **required** functionality is complete:
+
+* ✅ Game interface has a heading (h1 tag), a line of body text (p tag), and four buttons that match the demo app
+* ✅ "Start" button toggles between "Start" and "Stop" when clicked. 
+* ✅ Game buttons each light up and play a sound when clicked. 
+* ✅ Computer plays back sequence of clues including sound and visual cue for each button
+* ✅ Play progresses to the next turn (the user gets the next step in the pattern) after a correct guess. 
+* ✅ User wins the game after guessing a complete pattern
+* ✅ User loses the game after an incorrect guess
+
+The following **optional** features are implemented:
+
+* ✅ Any HTML page elements (including game buttons) has been styled differently than in the tutorial
+* ✅ Buttons use a pitch (frequency) other than the ones in the tutorial
+* ✅ More than 4 functional game buttons
+* ✅ Playback speeds up on each turn
+* ✅ Computer picks a different pattern each time the game is played
+* ❌ Player only loses after 3 mistakes (instead of on the first mistake)
+* ❌ Game button appearance change goes beyond color (e.g. add an image)
+* ❌ Game button sound is more complex than a single tone (e.g. an audio file, a chord, a sequence of multiple tones)
+* ✅ User has a limited amount of time to enter their guess on each turn
+
+
+## Video Walkthrough (GIF)
+<img src="gametime.gif" width=250><br>
+
+
+
+## Reflection Questions
+1. If you used any outside resources to help complete your submission (websites, books, people, etc) list them here. 
+stackoverflow,W3 Schools,
+
+2. What was a challenge you encountered in creating this submission (be specific)? How did you overcome it? (recommended 200 - 400 words) 
+The basic requirement for the project was very easy and straight forward the problem started from the bonus part of the assignemnt. The extra features required a bit extra time and some help from the online resources.
+The resources helped alot in building and understanding the concept.
+
+3. What questions about web development do you have after completing your submission? (recommended 100 - 300 words) 
+I have been working and learning the machine learning and other topics and was under estimating the power and area of scope of this domain.
+The web development lets you put all your imagination on screen and some times makes it real.I think there are many things above and beyond that i need still need learn. There is alot apart form the head body and paragraph tag.
+I need to learn alot and add skills .
+
+4. If you had a few more hours to work on this project, what would you spend them doing (for example: refactoring certain functions, adding additional features, etc). Be specific. (recommended 100 - 300 words) 
+If I had a bit more time to work on this project would have deifinetly worked on the three extra things which I have missed from the extra stuff and would have made some prefference in the difficulty of the game and would have played with some themes and sound options.
+There are alot more things that can be done and make the game very fun and intresting.
+
+
+
+## Interview Recording URL Link
+
+[My 5-minute Interview Recording](https://github.com/kuljotChadha/SITE_PREWORK/blob/main/VideoInterview.mp4)
+
+
+## License
+
+    Copyright [kuljot Singh]
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
---
+++ script.js
@@ -0,0 +1,177 @@
+//Global Variables
+var pattern = []; //to be filled at game start
+var patternLength = 8;
+var numBtns = 6;
+var progress = 0; 
+var gamePlaying = false;
+var tonePlaying = false;
+var volume = 0.5;  //must be between 0.0 and 1.0
+var guessCounter = 0;
+var freqMap = {} //to be filled at game start
+var clueHoldTime = 1000.0; //how long to hold each clue's light/sound
+var curScore = 0; //current score
+var highScore = 0; //high score
+var timer; //timer object
+var timeGiven = 10; //how much time to guess after clues stop
+var timeRemaining = 0; //how much time the player has left
+
+//Global constants
+const cluePauseTime = 333; //how long to pause in between clues
+const nextClueWaitTime = 1000; //how long to wait before starting playback of the clue sequence
+
+//General game functions
+function startGame(){
+  //initialize game variables
+  for (let i = 0; i < patternLength; i++) pattern[i] = Math.round(Math.random()*numBtns);
+  let startingFreq = Math.random()*120+120;
+  for (let i = 0; i < numBtns; i++) freqMap[i+1] = startingFreq+30*i;
+  progress = 0;
+  clueHoldTime = 1000.0;
+  gamePlaying = true;
+  curScore = 0;
+  // swap the Start and Stop buttons
+  document.getElementById("startBtn").classList.add("hidden");
+  document.getElementById("stopBtn").classList.remove("hidden");
+  
+  document.getElementById("volumeSlider").addEventListener("change", function() {
+    volume = document.getElementById("volumeSlider").value/100.0;
+    console.log("Volume updated to " + volume);
+}, false);
+  
+  playClueSequence();
+}
+function stopGame(){
+  gamePlaying = false;
+  // swap the Start and Stop buttons
+  document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("startBtn").classList.remove("hidden");
+  if (curScore > highScore) {
+    highScore = curScore;
+    updateParagraph();
+  }
+  clearTimer();
+}
+function loseGame(){
+  stopGame();
+  alert("Game Over. You lost.");
+}
+function winGame(){
+  stopGame();
+  alert("Game Over. You won.");
+}
+
+// Sound Synthesis Functions
+function playTone(btn,len){ 
+  o.frequency.value = freqMap[btn]
+  g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+  tonePlaying = true
+  setTimeout(function(){
+    stopTone()
+  },len)
+}
+function startTone(btn){
+  if(!tonePlaying){
+    o.frequency.value = freqMap[btn]
+    g.gain.setTargetAtTime(volume,context.currentTime + 0.05,0.025)
+    tonePlaying = true
+  }
+}
+function stopTone(){
+    g.gain.setTargetAtTime(0,context.currentTime + 0.05,0.025)
+    tonePlaying = false
+}
+
+//Clue Playing Functions
+function lightButton(btn){
+  document.getElementById("button"+btn).classList.add("lit")
+}
+function clearButton(btn){
+  document.getElementById("button"+btn).classList.remove("lit")
+}
+function playSingleClue(btn){
+  if(gamePlaying){
+    lightButton(btn);
+    playTone(btn,clueHoldTime);
+    setTimeout(clearButton,clueHoldTime,btn);
+  }
+}
+function playClueSequence(){
+  let delay = nextClueWaitTime; //set delay to initial wait time
+  guessCounter = 0;
+  clueHoldTime *= 0.9;
+  clearTimeout(timer);
+  document.getElementById("p2").innerHTML = "Time remaining: " + timeGiven;
+  for(let i=0;i<=progress;i++){ // for each clue that is revealed so far
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms")
+    setTimeout(playSingleClue,delay,pattern[i]) // set a timeout to play that clue
+    delay += clueHoldTime 
+    delay += cluePauseTime;
+  }
+  
+  timeRemaining = timeGiven;
+  timer = setTimeout(function tick() {
+    if (gamePlaying) {
+      updateTimer();
+      timer = setTimeout(tick, 1000); // (*)
+    }
+  }, delay);
+}
+
+function updateParagraph() {
+    document.getElementById("p1").innerHTML = "Select buttons in the  order as they appear to win.\nCurrent Score: "+curScore+" | High Score: "+highScore;
+}
+function clearTimer() {
+  clearTimeout(timer);
+  timeRemaining = 0;
+  document.getElementById("p2").innerHTML = "";
+}
+function updateTimer() {
+    if (timeRemaining >= 0) {
+      document.getElementById("p2").innerHTML = "Time remaining to Answer: " + timeRemaining;
+      timeRemaining--;
+    } else {
+      loseGame();
+    }
+}
+
+//User input functions
+function guess(btn){
+  console.log("user guessed: " + btn);
+  if(!gamePlaying){
+    return;
+  }
+
+  // add game logic here
+  if (btn == pattern[guessCounter]) {
+    if (guessCounter == progress) {
+      progress++;
+      curScore = progress;
+      updateParagraph();
+      if (progress == patternLength) {
+        winGame();
+        return;
+      }
+      playClueSequence();
+    } else {
+      guessCounter++;
+    }
+  } else {
+    loseGame();
+    return;
+  }
+}
+
+//Page Initialization
+// Init Sound Synthesizer
+var context = new AudioContext()
+var o = context.createOscillator()
+var g = context.createGain()
+g.connect(context.destination)
+g.gain.setValueAtTime(0,context.currentTime)
+o.connect(g)
+o.start(0)
+updateParagraph();
+//Initialization so that the buttons aren't useless before the game starts
+for (let i = 0; i < patternLength; i++) pattern[i] = Math.round(Math.random()*numBtns);
+let startingFreq = Math.random()*120+120;
+for (let i = 0; i < numBtns; i++) freqMap[i+1] = startingFreq+30*i;
